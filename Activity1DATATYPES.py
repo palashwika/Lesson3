82c1c5dbd4173dbadc06dfc1afa73ddc42996948
palashwika <paladhiashwika@gmail.com>
2025-07-02 10:37:24 -0700

    Adding lesson 3 activities

---
+++ Activity1DATATYPES.py
@@ -0,0 +1,12 @@
+#Let's check the datatype of different values
+a=19
+print("type of a:", type(a))
+
+b=3.14
+print("type of b:", type(b))
+
+c="coding"
+print("type of c:", type(c))
+
+d=False
+print("type of d:", type(d))
---
+++ Typecasting.py
@@ -0,0 +1,27 @@
+#Assigning Different variables
+name="Ashwika"
+age=15
+dancer=True
+weight=10.6
+
+#Printing Different Variables and their Data Type
+print("Name:",name)
+print("Data Type of Age is", type(name))
+
+print("Age:", age)
+print("Data type of age is", type(age))
+
+print("dancer:", dancer)
+print("Data Type of dancer is", type(dancer))
+
+print("Weight:",weight)
+print("Data Type of weight is", type(weight))
+
+#Type casting to convert the datatype of variables
+print("\n After Type Casting ....")
+age=str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is",type(weight))
